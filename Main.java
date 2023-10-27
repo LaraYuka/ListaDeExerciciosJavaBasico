e658906ec5501a234b8b20c2809ee6309df476bf
LaraYuka <85855158+LaraYuka@users.noreply.github.com>
2023-10-27 14:10:26 +0000

    Exercicio 01

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-    System.out.println("Teste");
+    
+    Exercicios ex = new Exercicios();
+
+    ex.Ex1();
   }
+
 }
